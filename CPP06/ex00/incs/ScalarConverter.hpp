fbdbe7d4babb0287ec4116b73e70d6b56de720fd
Casey Burgess <caburges@f6r10s9.clusters.42paris.fr>
2025-06-25 16:22:49 +0200

    finished ex00

--- CPP06/ex00/incs/ScalarConverter.hpp
+++ CPP06/ex00/incs/ScalarConverter.hpp
@@ -18,5 +18,12 @@ bool	isChar(const std::string& input);
 bool	isPseudoLiteral(const std::string& input);
 bool	isInt(const std::string& input);
 bool	isDouble(const std::string& input);
+bool	isFloat(const std::string& input);
+void	convertChar(const std::string input);
+void	convertInt(const std::string& input);
+void	convertDouble(const std::string& input);
+void	convertFloat(const std::string& input);
+void	printImpossible(void);
+void	convertPseudoLiteral(const std::string& input);
 
 #endif
--- CPP06/ex00/srcs/ScalarConverter.cpp
+++ CPP06/ex00/srcs/ScalarConverter.cpp
@@ -1,9 +1,9 @@
 #include "ScalarConverter.hpp"
-#include <limits.h>
+#include <limits>
 #include <iostream>
 #include <bits/stdc++.h>
 #include <cstdlib>
-#include <errno.h>
+#include <cerrno>
 
 ScalarConverter::ScalarConverter(void)
 {
@@ -28,24 +28,164 @@ ScalarConverter::~ScalarConverter(void)
 
 void	ScalarConverter::convert(const std::string input)
 {
-	// detect the type passed
-	// convert it to its proper type
-	// explicitly convert it to the three other data types
-	// print out the results
 	if (isPseudoLiteral(input))
-		std::cout << input << " is pseudo literal." << std::endl; //convertPseudoLiteral(input);
+		convertPseudoLiteral(input);
 	else if (isChar(input))
-		std::cout << input << " is char." << std::endl; //convertChar(input);
+		convertChar(input);
 	else if (isInt(input))
-		std::cout << input << " is int." << std::endl; //convertChar(input);
+		convertInt(input);
 	else if (isDouble(input))
-		std::cout << input << " is double." << std::endl; //convertChar(input);
+		convertDouble(input);
+	else if (isFloat(input))
+		convertFloat(input);
 	else
-		std::cout << input << " is not recognised" << std::endl;
-//	else if (isFloat(input))
-//		convertFloat(input);
-//	else
-//		convertImpossible();
+		printImpossible();
+}
+
+void	printImpossible(void)
+{
+	std::cout << "char: " << "impossible" << std::endl;
+	std::cout << "int: " << "impossible" << std::endl;
+	std::cout << "double: " << "impossible" << std::endl;
+	std::cout << "float: " << "impossible" << std::endl;
+}
+
+void	convertChar(const std::string input)
+{
+	char c = input[0];
+	int i = static_cast<int>(c);
+	double d = static_cast<double>(c);
+	float f = static_cast<float>(c);
+	
+	std::cout << "char: '" << c << "'" << std::endl;
+	std::cout << "int: " << i << std::endl;
+	std::cout << "double: " << std::setprecision(1) << std::fixed << d << std::endl;
+	std::cout << "float: " << std::setprecision(1) << std::fixed << f << "f" << std::endl;
+}
+
+void	convertInt(const std::string& input)
+{
+	char *endptr;
+	long int result = std::strtol(input.c_str(), &endptr, 10);
+	if (result < INT_MIN || result > INT_MAX)
+	{
+		printImpossible();
+		return ;
+	}
+	int i = std::atoi(input.c_str());
+	if (!isascii(i))
+		std::cout << "char: impossible" << std::endl;
+	else if (isascii(i) && isprint(i))
+	{
+		char c = static_cast<char>(i);
+		std::cout << "char: " << "'" << c << "'" << std::endl;
+	}
+	else
+		std::cout << "char: Non displayable" << std::endl;
+
+	std::cout << "int: " << i << std::endl;
+	double d = static_cast<double>(i);
+	float f = static_cast<float>(i);
+	std::cout << "double: " << std::setprecision(1) << std::fixed << d << std::endl;
+	std::cout << "float: " << std::setprecision(1) << std::fixed << f << "f" << std::endl;
+}
+
+int	getPrecision(const std::string& input)
+{
+	size_t dotPos = input.find('.') + 1;
+        int i = 0;
+	while (dotPos < input.length() && isdigit(input[dotPos]))
+	{
+		i++;
+		dotPos++;
+	}
+	if (i == 0)
+		return (1);
+	return (i);
+}
+
+void	convertFloat(const std::string& input)
+{
+	char* endptr;
+	errno = 0;
+	double f = std::strtof(input.c_str(), &endptr);
+	if (errno == ERANGE)
+	{
+		printImpossible();
+		return ;
+	}
+	if (!isascii(f))
+		std::cout << "char: impossible" << std::endl;
+	else if (isascii(f) && isprint(f))
+	{
+		char c = static_cast<char>(f);
+		std::cout << "char: " << "'" << c << "'" << std::endl;
+	}
+	else
+		std::cout << "char: Non displayable" << std::endl;
+	if (f < INT_MIN || f > INT_MAX)
+		std::cout << "int: impossible" << std::endl;
+	else
+	{
+		int i = static_cast<int>(f);
+		std::cout << "int: " << i << std::endl;
+	}
+	double d = static_cast<double>(f);
+	std::cout << "double: " << std::setprecision(getPrecision(input)) << std::fixed << d << std::endl;
+	std::cout << "float: " << std::setprecision(getPrecision(input)) << std::fixed << f << "f" << std::endl;
+}
+
+void	convertDouble(const std::string& input)
+{
+	char* endptr;
+	errno = 0;
+	double d = std::strtod(input.c_str(), &endptr);
+	if (errno == ERANGE)
+	{
+		printImpossible();
+		return ;
+	}
+	if (!isascii(d))
+		std::cout << "char: impossible" << std::endl;
+	else if (isascii(d) && isprint(d))
+	{
+		char c = static_cast<char>(d);
+		std::cout << "char: " << "'" << c << "'" << std::endl;
+	}
+	else
+		std::cout << "char: Non displayable" << std::endl;
+	if (d < INT_MIN || d > INT_MAX)
+		std::cout << "int: impossible" << std::endl;
+	else
+	{
+		int i = static_cast<int>(d);
+		std::cout << "int: " << i << std::endl;
+	}
+	std::cout << "double: " << std::setprecision(getPrecision(input)) << std::fixed << d << std::endl;
+	if (d < FLT_MIN || d > FLT_MAX)
+		std::cout << "float: impossible" << std::endl;
+	else
+	{
+		float f = static_cast<float>(d);
+		std::cout << "float: " << std::setprecision(getPrecision(input)) << std::fixed << f << "f" << std::endl;
+	}
+}
+
+bool	isFloat(const std::string& input)
+{
+	size_t found = input.find(".");
+	if (found == std::string::npos)
+		return (false);
+	if (input[input.length() - 1] != 'f')
+		return (false);
+	std::string trunc = input;
+	trunc[input.length() - 1] = '\0';
+	char* endptr;
+	float result = std::strtof(trunc.c_str(), &endptr);
+	(void)result;
+	if (*endptr != '\0')
+		return (false);
+	return (true);
 }
 
 bool	isDouble(const std::string& input)
@@ -57,33 +197,19 @@ bool	isDouble(const std::string& input)
 	if (found != std::string::npos)
 		return (false);
 	char* endptr;
-	errno = 0;
 	double result = std::strtod(input.c_str(), &endptr);
-	std::cout << result << std::endl;
+	(void)result;
 	if (*endptr != '\0')
 		return (false);
-	if (errno == ERANGE)
-		return (false);
 	return (true);
-
 }
 
 bool	isInt(const std::string& input)
 {
-	int len = input.length();
 	char* endptr;
-	int i = 0;
-	if (input[i] == '+' || input[i] == '-')
-		i++;
-	if (i == len)
-		return (false);
-	for (int j = i; j < len; j++)
-	{
-		if (!isdigit(input[j]))
-			return (false);
-	}
 	long int result = std::strtol(input.c_str(), &endptr, 10);
-	if (result < INT_MIN || result > INT_MAX)
+	(void)result;
+	if (*endptr != '\0')
 		return (false);
 	return (true);
 }
@@ -93,6 +219,28 @@ bool	isChar(const std::string& input)
 	if (input.length() != 1 || !std::isprint(input[0]) || std::isdigit(input[0]))
 		return (false);
 	return (true);
+}
+
+void	convertPseudoLiteral(const std::string& input)
+{
+	std::cout << "char: " << "impossible" << std::endl;
+	std::cout << "int: " << "impossible" << std::endl;
+	
+	char *endptr;
+	float f;
+	double d;
+	if (input == "-inff" || input == "+inff" || input == "nanf")
+	{
+		f = std::strtof(input.c_str(), &endptr);
+		d = static_cast<double>(f);
+	}
+	else
+	{
+		d = std::strtod(input.c_str(), &endptr);
+		f = static_cast<float>(d);
+	}
+	std::cout << "float: " << f << "f" << std::endl;
+	std::cout << "double: " << d << std::endl;
 }
 
 bool	isPseudoLiteral(const std::string& input)
@@ -106,9 +254,3 @@ bool	isPseudoLiteral(const std::string& input)
 	}
 	return (false);
 }
-
-
-
-//int atoi(str.c_str())
-//float std::stof
-//double std::stod / atof
