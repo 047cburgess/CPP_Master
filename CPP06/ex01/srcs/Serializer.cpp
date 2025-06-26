9bf52b5828164176c29827885130ac50eda73ea3
Casey Burgess <caburges@f0r7s21.clusters.42paris.fr>
2025-06-26 13:40:12 +0200

    ex 01 done

--- CPP06/ex01/srcs/Serializer.cpp
+++ CPP06/ex01/srcs/Serializer.cpp
@@ -27,10 +27,10 @@ Serializer::~Serializer(void)
 
 uintptr_t	Serializer::serialize(Data* ptr)
 {
-
+	return (reinterpret_cast<uintptr_t>(ptr));
 }
 
 Data* 		Serializer::deserialize(uintptr_t raw)
 {
-
+	return (reinterpret_cast<Data*>(raw));
 }
--- CPP06/ex01/srcs/main.cpp
+++ CPP06/ex01/srcs/main.cpp
@@ -3,6 +3,17 @@
 #include <iostream>
 # include <stdint.h>
 
+
+std::ostream& operator<<(std::ostream& os, const Data& other)
+{
+	os << other.i << std::endl;
+	os << other.c << std::endl;
+	os << other.s << std::endl;
+	os << other.d << std::endl;
+	os << other.f << std::endl;
+	return (os);
+}
+
 int	main(void)
 {
 	Data my_data;
@@ -13,8 +24,19 @@ int	main(void)
 	my_data.d = 42.2;
 	my_data.f = 24.2f;
 
-	uintptr_t	ptr = reinterpret_cast<uintptr_t>(&my_data);
+	std::cout << "---my_data---" << std::endl;
+	std::cout << my_data << std::endl;
+	uintptr_t forward = Serializer::serialize(&my_data);
 
-	
-	Data* returned = reinterpret_cast<Data>(ptr);
+	Data* back = Serializer::deserialize(forward);
+	std::cout << "---pointer after serialize + deserialize my_data---" << std::endl;
+	std::cout << *back << std::endl;
+
+	std::cout << "---changing i to 20, s to 'hello world'---" << std::endl;
+	my_data.i = 20;
+	my_data.s = "hello world";
+	std::cout << "---my_data---" << std::endl;
+	std::cout << my_data << std::endl;
+	std::cout << "---pointer to my_data---" << std::endl;
+	std::cout << *back << std::endl;
 }
