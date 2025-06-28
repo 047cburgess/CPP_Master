07aa83276e3754afa2ca0c555deb7f7d372ca6fe
Casey Burgess <caburges@f6r6s5.clusters.42paris.fr>
2025-06-28 20:07:15 +0200

    ex01 done

---
+++ CPP07/ex01/srcs/main.cpp
@@ -0,0 +1,37 @@
+#include "iter.hpp"
+#include <string>
+#include <iostream>
+
+template <typename T>
+void	print(T& a)
+{
+	std::cout << a;
+}
+
+template <typename T>
+void	increment(T& i)
+{
+	i++;
+}
+
+int	main(void)
+{
+	int intArray[5] = {1, 2, 3, 4, 5};
+	::iter(intArray, 5, print);
+	std::cout << std::endl;
+
+	const int intArrayConst[5] = {1, 2, 3, 4, 5};
+	::iter(intArrayConst, 5, print);
+	std::cout << std::endl;
+
+	::iter(intArray, 5, increment);
+	::iter(intArray, 5, print);
+	std::cout << std::endl;
+
+	char charArray[5] = {'a', 'b', 'c', 'd', 'e'};
+	::iter(charArray, 5, print);
+	std::cout << std::endl;
+	::iter(charArray, 5, increment);
+	::iter(charArray, 5, print);
+	std::cout << std::endl;
+}
