6a6d47551b86eeb69aea8486eb64db82f08ec8a6
Casey Burgess <caburges@f6r5s5.clusters.42paris.fr>
2025-06-30 13:21:16 +0200

    updated for repush

--- CPP07/ex01/incs/iter.hpp
+++ CPP07/ex01/incs/iter.hpp
@@ -1,10 +1,19 @@
 #pragma once
 
-template <typename T>
-void	iter(T* array, unsigned int size, void (*f)(T&))
+template <typename T, typename F>
+void	iter(T* array, unsigned int size, F f)
 {
 	if (!array || !f)
 		return ;
 	for (unsigned int i = 0; i < size; i++)
 		(*f)(array[i]);
 }
+
+//template <typename T>
+//void	iter(T* array, unsigned int size, void (*f)(const T&))
+//{
+//	if (!array || !f)
+//		return ;
+//	for (unsigned int i = 0; i < size; i++)
+//		(*f)(array[i]);
+//}
